540212fc1c78fc8be6e7a7b4bb6ab94f3bdfd592
Slavcho Cvetanovski <cvetanovski.slavco@gmail.com>
2023-03-28 21:47:06 +0200

    Improving development workflow for external libraries
    This MR adds craco config for enabling hot reload when using external local libraries.
    It adds webpack alias for resolving all common packages that libraries are using and imports the component library
    from your local machine to the application it self.
    It adds ts-loader to resolve all typescript errors when using ts files outside of the src folder.
    
    USAGE
    - open the craco.config.js
    - set the ENABLE_LOCAL_LIB_HOT_RELOAD to true
    - add the relative path to the components library in COMPONENTS_LIBRARY_PATH
    - run yarn start
    
    Closes #1066

--- app/craco.config.js
+++ app/craco.config.js
@@ -4,11 +4,34 @@
 
 const CracoEsbuildPlugin = require('craco-esbuild');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const path = require('path')
+
+/* ENABLE HOT RELOADING FEATURE
+ - Setting ENABLE_LOCAL_LIB_HOT_RELOAD to true will connect this app directly to the local library and
+   enable hot reload
+*/
+const ENABLE_LOCAL_LIB_HOT_RELOAD = false
+/* COMPONENTS_LIBRARY_PATH
+ - Set your local path to the component library
+*/
+const COMPONENTS_LIBRARY_PATH = '../../module-component'
+
 
 module.exports = ({ env }) => {
     const isProductionBuild = process.env.NODE_ENV === "production"
     const analyzerMode = process.env.REACT_APP_INTERACTIVE_ANALYZE
         ? "server" : "json";
+	const externalLibraryResolver = {
+				react: path.resolve(__dirname, "../node_modules/react"),
+				"react-dom": path.resolve(__dirname, "../node_modules/react-dom"),
+				"react-redux": path.resolve(__dirname, "../node_modules/react-redux"),
+				"@opentalk/notistack": path.resolve(__dirname, "../node_modules/@opentalk/notistack"),
+				"@opentalk/common": path.resolve(__dirname, "../node_modules/@opentalk/common"),
+				"@mui/styles": path.resolve(__dirname, "../node_modules/@mui/styles"),
+				"@mui/material": path.resolve(__dirname, "../node_modules/@mui/material"),
+				"react-i18next": path.resolve(__dirname, "../node_modules/react-i18next"),
+				"@opentalk/components": path.resolve(__dirname, COMPONENTS_LIBRARY_PATH)
+	}
 
     return {
         plugins: [
@@ -24,6 +47,26 @@ module.exports = ({ env }) => {
             plugins: {
                 add: isProductionBuild ? [new BundleAnalyzerPlugin({ analyzerMode })] : []
             },
+			alias: !isProductionBuild && ENABLE_LOCAL_LIB_HOT_RELOAD ?  externalLibraryResolver : {},
+			configure: webpackConfig => {
+				const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+				({ constructor }) => constructor && constructor.name === "ModuleScopePlugin",
+				);
+
+				webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+
+				webpackConfig.module.rules.push({
+					test: /\.tsx?$/,
+					loader: 'ts-loader',
+					exclude: /node_modules/,
+					options: {
+						transpileOnly: true,
+						configFile: 'tsconfig.json',
+					},
+				})
+
+				return webpackConfig;
+			},
         },
 		jest: {
 			configure: (jestConfig) => {
