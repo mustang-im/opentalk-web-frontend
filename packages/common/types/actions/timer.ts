1ad2af5f63c21035185628239356e0efae813dc4
Velin Staikov <velin.s@droxic.com>
2023-08-01 14:09:43 +0300

    Add extra reducers to control hotkeys state
    Move timer related types/actions into common so they can be used in
    hotkeys slice.
    Update TimerStopped type to match backend.

---
+++ packages/common/types/actions/timer.ts
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
+import { ParticipantId, Timestamp } from "../common";
+import { TimerKind, TimerStopKind, TimerStyle } from "../timer";
+
+export interface TimerStarted {
+  message: 'started';
+  timerId: string;
+  readyCheckEnabled: boolean;
+  endsAt?: Timestamp;
+  title?: string;
+  startedAt: Timestamp;
+  kind?: TimerKind;
+  style?: TimerStyle;
+}
+  
+export interface TimerStopped {
+  message: 'stopped';
+  kind: TimerStopKind;
+  /**
+   * When kind === by_moderator
+   * The participant id of the moderator that stopped the timer
+  */
+  participantId?: ParticipantId;
+  /**
+   * When kind === by_moderator
+   * Optional reason. Set by the moderator
+  */
+  reason?: string;
+}
