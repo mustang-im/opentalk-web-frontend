13e701ae2dad04845d5827a9bb9b87f32f3f02e3
Stefan L <stefan.l@droxic.com>
2023-08-30 11:29:18 +0200

    fix licensing

--- app/src/components/Participants/fragments/ParticipantsContainer.tsx
+++ app/src/components/Participants/fragments/ParticipantsContainer.tsx
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
 import { memo, useCallback } from 'react';
 
 import { useAppDispatch, useAppSelector } from '../../../hooks';
