a2b10b2e45fb69ae32d38f11c5d453f266afdc30
Stefan L <stefan.l@droxic.com>
2024-01-11 13:29:43 +0100

    narrow down form position in the structure

--- app/src/components/LobbyView/LobbyView.tsx
+++ app/src/components/LobbyView/LobbyView.tsx
@@ -55,6 +55,8 @@ const showWrongPasswordNotification = () => {
   });
 };
 
+const JOIN_FORM_ID = 'join-form';
+
 const LobbyView: FC = () => {
   const dispatch = useAppDispatch();
   const mediaContext = useMediaContext();
@@ -181,24 +183,25 @@ const LobbyView: FC = () => {
 
   return (
     <>
-      <form onSubmit={formik.handleSubmit}>
-        <Container>
-          <Stack direction={'column'} spacing={4} justifyContent={'center'} alignItems={'center'}>
-            <SelfTest
-              actionButton={
-                <Button
-                  type={'submit'}
-                  disabled={
-                    !(isLoggedIn || inviteCode !== undefined) ||
-                    connectionState === ConnectionState.Starting ||
-                    !formik.isValid
-                  }
-                >
-                  {t('joinform-enter-now')}
-                </Button>
-              }
-              title={roomData?.title}
-            >
+      <Container>
+        <Stack direction={'column'} spacing={4} justifyContent={'center'} alignItems={'center'}>
+          <SelfTest
+            actionButton={
+              <Button
+                form={JOIN_FORM_ID}
+                type={'submit'}
+                disabled={
+                  !(isLoggedIn || inviteCode !== undefined) ||
+                  connectionState === ConnectionState.Starting ||
+                  !formik.isValid
+                }
+              >
+                {t('joinform-enter-now')}
+              </Button>
+            }
+            title={roomData?.title}
+          >
+            <form id={JOIN_FORM_ID} onSubmit={formik.handleSubmit}>
               <Stack direction={'row'} spacing={1}>
                 <ContitionalToolTip
                   showToolTip={Boolean(disableDisplayNameField)}
@@ -234,11 +237,12 @@ const LobbyView: FC = () => {
                   />
                 )}
               </Stack>
-            </SelfTest>
-          </Stack>
-        </Container>
-      </form>
-      {showImprintContainer && <ImprintContainer />}
+            </form>
+          </SelfTest>
+        </Stack>
+      </Container >
+      {showImprintContainer && <ImprintContainer />
+      }
     </>
   );
 };
